7811deb90e7fc638d53886fc1eecbb11e34b75bd
Sursulet <57486825+Sursulet@users.noreply.github.com>
2020-10-21 15:40:10 +0200

    Add Drawer Navigation

--- app/src/main/java/com/sursulet/go4lunch/MainActivity.java
+++ app/src/main/java/com/sursulet/go4lunch/MainActivity.java
@@ -1,20 +1,31 @@
 package com.sursulet.go4lunch;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.sursulet.go4lunch.ui.DetailsActivity;
+import com.sursulet.go4lunch.ui.SettingsActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    AppBarConfiguration mAppBarConfig;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +35,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         BottomNavigationView bottom = findViewById(R.id.bottom_nav);
 
-        mAppBarConfig = new AppBarConfiguration.Builder(
+        AppBarConfiguration mAppBarConfig = new AppBarConfiguration.Builder(
                 R.id.action_map, R.id.action_list, R.id.action_workmates)
                 .build();
         NavController navCtrl = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -38,5 +58,34 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_your_lunch:
+                startActivity(new Intent(getApplicationContext(), DetailsActivity.class));
+                break;
+
+            case R.id.action_settings:
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                break;
+
+            case R.id.action_logout:
+                Toast.makeText(this, "LOGOUT", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/sursulet/go4lunch/ui/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.sursulet.go4lunch.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sursulet.go4lunch.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
