3fe6dd813cad80c94433b2d00f1086500c12b3cf
Sursulet <57486825+Sursulet@users.noreply.github.com>
2020-10-28 19:26:54 +0100

    Creating classes for the Map

---
+++ app/src/main/java/com/sursulet/go4lunch/model/GooglePlacesNearbySearchResult.java
@@ -0,0 +1,44 @@
+
+package com.sursulet.go4lunch.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class GooglePlacesNearbySearchResult {
+
+    @SerializedName("next_page_token")
+    @Expose
+    private String nextPageToken;
+    @SerializedName("results")
+    @Expose
+    private List<Result> results = null;
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    public String getNextPageToken() {
+        return nextPageToken;
+    }
+
+    public void setNextPageToken(String nextPageToken) {
+        this.nextPageToken = nextPageToken;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
